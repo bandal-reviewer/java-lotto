2eda086922bf892c21b1149c229968461775c0aa
2384320 <chdms1549@gmail.com>
2023-07-01 15:33:49 +0900

    feat: 로또 발행 기능 추가
    - 출력을 위해 Lotto 객체에 getNumbers 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,16 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
         int purchasePrice = inputPurchasePrice();
+        Lotto[] lotteries = getLotteryTickets(purchasePrice);
     }
 
     public static int inputPurchasePrice() {
@@ -14,4 +20,24 @@ public class Application {
 
         return price;
     }
+
+    public static Lotto[] getLotteryTickets(int purchasePrice) {
+        int purchaseLotteryCount = purchasePrice / 1000;
+        System.out.println(purchaseLotteryCount + "개를 구매했습니다.");
+
+        Lotto[] lotteries = new Lotto[purchaseLotteryCount];
+        for (int i = 0; i < purchaseLotteryCount; i++) {
+            lotteries[i] = new Lotto(getRandomNumberList());
+            System.out.println(lotteries[i].getNumbers());
+        }
+        return lotteries;
+    }
+
+    public static List<Integer> getRandomNumberList() {
+        List<Integer> list = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(list);
+        return list;
+    }
+
+
 }
