a5a1c72531a063f1fac47380528df134ebf96441
2384320 <chdms1549@gmail.com>
2023-07-01 15:19:05 +0900

    feat: 구입금액 입력받는 기능 추가
    - 구입 금액 입력받는 기능 추가
    - 구입 금액이 1000에 나누어 떨어지지 않는 수인지 검사하는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int purchasePrice = inputPurchasePrice();
+    }
+
+    public static int inputPurchasePrice() {
+        System.out.println("구입금액을 입력해 주세요.");
+        int price = Integer.parseInt(readLine());
+        if (price % 1000 != 0) throw new IllegalArgumentException();
+
+        return price;
     }
 }
