3c3129df8fbb4721d58cbc9311784051e8514251
2384320 <chdms1549@gmail.com>
2023-07-04 21:18:33 +0900

    refactor: Winning outputString 삭제 및 상수명 수정

--- src/main/java/lotto/Domain/Winning.java
+++ src/main/java/lotto/Domain/Winning.java
@@ -6,20 +6,18 @@ import lotto.UI.Output;
 import java.util.Map;
 
 public enum Winning {
-    NO_WINNING(-1, 0, ""),
-    THREE_WINNING(3, 5, "3개 일치 (5,000원) - "),
-    FOUR_WINNING(4, 50, "4개 일치 (50,000원) - "),
-    FIVE_WINNING(5, 1_500, "5개 일치 (1,500,000원) - "),
-    FIVE_AND_BONUS_WINNING(6, 30_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    SIX_WINNING(6, 2_000_000, "6개 일치 (2,000,000,000원) - ");
+    NO_WINNING(-1, 0),
+    FIFTH(3, 5),
+    FOURTH(4, 50),
+    THIRD(5, 1_500),
+    SECOND(6, 30_000),
+    FIRST(6, 2_000_000);
 
-    private int correctCount, moneyRatio;
-    private String outputString;
+    private final int correctCount, moneyRatio;
 
-    Winning(int correctCount, int moneyRatio, String outputString) {
+    Winning(int correctCount, int moneyRatio) {
         this.correctCount = correctCount;
         this.moneyRatio = moneyRatio;
-        this.outputString = outputString;
     }
 
     public int getCorrectCount() {
@@ -30,17 +28,13 @@ public enum Winning {
         return moneyRatio;
     }
 
-    public String getOutputString() {
-        return outputString;
-    }
-
     public static Winning getRightWinningScore(int winningCount, boolean hasBonusNum) {
-        if (winningCount == FIVE_AND_BONUS_WINNING.correctCount
-                && hasBonusNum) return FIVE_AND_BONUS_WINNING;
+        if (winningCount == SECOND.correctCount
+                && hasBonusNum) return SECOND;
 
         for (Winning winning : values()) {
             if (winningCount == winning.getCorrectCount()
-                    && winning != FIVE_AND_BONUS_WINNING) return winning;
+                    && winning != SECOND) return winning;
         }
 
         return NO_WINNING;
@@ -50,7 +44,7 @@ public enum Winning {
         Output.printResultView();
         for (Winning winning : values()) {
             if (winning == NO_WINNING) continue;
-            Output.printWinningScore(winning.getOutputString(), winningScoreMap.get(winning));
+            Output.printWinningScore(winning.name(), winningScoreMap.get(winning));
         }
         Output.printRateOfReturn(rateOfReturn);
     }
