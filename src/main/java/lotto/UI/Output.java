78b20345c3bb9d028a6ad5a56799cee0fa8b7b3b
2384320 <chdms1549@gmail.com>
2023-07-05 21:53:39 +0900

    style: 들여쓰기 삭제

--- src/main/java/lotto/UI/Output.java
+++ src/main/java/lotto/UI/Output.java
@@ -50,16 +50,13 @@ public class Output {
 
     public static void outputPurchasePrice() {
         System.out.println("구입금액을 입력해 주세요.");
-
     }
 
     public static void outputWinningNumbers() {
         System.out.println("당첨 번호를 입력해주세요.");
-
     }
 
     public static void outputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
-
     }
 }
