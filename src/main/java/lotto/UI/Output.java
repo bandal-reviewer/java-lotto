c36ac885866428d67185ac4ccb20e302d84d1d4c
2384320 <chdms1549@gmail.com>
2023-07-05 19:54:19 +0900

    refactor: 에러 메시지 상수화 해제

--- src/main/java/lotto/UI/Output.java
+++ src/main/java/lotto/UI/Output.java
@@ -5,13 +5,6 @@ import lotto.Domain.Winning;
 import java.util.List;
 
 public class Output {
-    private static final String ERROR_PURCHASE_PRICE_INTEGER = "[ERROR] 구입금액은 숫자로 입력해야 합니다.";
-    private static final String ERROR_PURCHASE_PRICE_DIVIDE = "[ERROR] 구입금액은 1,000에 나누어 떨어지는 숫자여야 합니다.";
-    private static final String ERROR_WINNING_NUMBERS_FORMAT = "[ERROR] 당첨 번호는 띄어쓰기 없이 ,로 구분하여 작성해야 합니다.";
-    private static final String ERROR_WINNING_NUMBERS_SIZE = "[ERROR] 번호는 6개만 작성되어야 합니다.";
-    private static final String ERROR_NUMBER_DUPLICATE = "[ERROR] 번호는 중복 숫자가 없어야 합니다.";
-    private static final String ERROR_NUMBER_RANGE =  "[ERROR] 번호의 범위는 1부터 45까지여야 합니다.";
-    private static final String ERROR_BONUS_NUMBER_FORMAT = "[ERROR] 보너스 번호는 숫자로 입력해야 합니다.";
     private static final String PURCHASE_LOTTERY = "개를 구매했습니다.";
     private static final String RESULT_VIEW = "당첨 통계\n---";
     private static final String RESULT_BONUS_CORRECT = ", 보너스 볼 일치 ";
@@ -48,30 +41,30 @@ public class Output {
     }
 
     public static void outputPurchasePriceInteger() {
-        System.out.println(ERROR_PURCHASE_PRICE_INTEGER);
+        System.out.println("[ERROR] 구입금액은 숫자로 입력해야 합니다.");
     }
 
     public static void outputPurchasePriceDivide() {
-        System.out.println(ERROR_PURCHASE_PRICE_DIVIDE);
+        System.out.println("[ERROR] 구입금액은 1,000에 나누어 떨어지는 숫자여야 합니다.");
     }
 
     public static void outputWinningNumbersFormat() {
-        System.out.println(ERROR_WINNING_NUMBERS_FORMAT);
+        System.out.println("[ERROR] 당첨 번호는 띄어쓰기 없이 ,로 구분하여 작성해야 합니다.");
     }
 
     public static void outputWinningNumbersSize() {
-        System.out.println(ERROR_WINNING_NUMBERS_SIZE);
+        System.out.println("[ERROR] 번호는 6개만 작성되어야 합니다.");
     }
 
     public static void outputBonusNumberFormat() {
-        System.out.println(ERROR_BONUS_NUMBER_FORMAT);
+        System.out.println("[ERROR] 보너스 번호는 숫자로 입력해야 합니다.");
     }
 
     public static void outputNumberDuplicate() {
-        System.out.println(ERROR_NUMBER_DUPLICATE);
+        System.out.println("[ERROR] 번호는 중복 숫자가 없어야 합니다.");
     }
 
     public static void outputNumberRange() {
-        System.out.println(ERROR_NUMBER_RANGE);
+        System.out.println("[ERROR] 번호의 범위는 1부터 45까지여야 합니다.");
     }
 }
