2614974ed59e5b6394bf58eafdbc8aa2a46114f3
2384320 <chdms1549@gmail.com>
2023-07-05 21:59:05 +0900

    refactor: 함수명 변경

--- src/main/java/lotto/Domain/WinningLotto.java
+++ src/main/java/lotto/Domain/WinningLotto.java
@@ -7,13 +7,13 @@ public class WinningLotto {
     private final Lotto winningLotto;
 
     public WinningLotto(int bonusNumber, Lotto winningLotto) {
-        validate(bonusNumber, winningLotto);
+        validateDuplicate(bonusNumber, winningLotto);
         winningLotto.getNumbers().add(bonusNumber);
         this.bonusNumber = new LottoNumber(bonusNumber);
         this.winningLotto = winningLotto;
     }
 
-    private void validate(int bonusNumber, Lotto winningLotto) {
+    private void validateDuplicate(int bonusNumber, Lotto winningLotto) {
         if (winningLotto.getNumbers().contains(bonusNumber))
             throw new IllegalArgumentException("[ERROR] 번호는 중복 숫자가 없어야 합니다.");
     }
